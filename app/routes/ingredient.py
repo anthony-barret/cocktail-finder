ecf986eaf7039d2be4f4fc4170829fa931fad16c
Anthony BARRET <47778402+anthony-barret@users.noreply.github.com>
2025-06-23 23:24:39 +0200

    feat: :sparkles: add route GET /ingredient/{ingredient_id}

--- app/routes/ingredient.py
+++ app/routes/ingredient.py
@@ -2,6 +2,13 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+# TODO: list ingredients
 @router.get("/ingredients")
 async def list_ingredients():
     pass
+
+# TODO: get ingredient
+@router.get("/ingredient/{ingredient_id}")
+async def get_ingredient(ingredient_id):
+    pass
+
